028a486f3d4b464b0869de686a4fc2edc1540617
yukio <zviamontevilca@gmail.com>
2024-06-29 07:50:24 -0500

    agregando descargador de archivos
    descargador de archivos en formato excel agregado

--- vista/editor.php
+++ vista/editor.php
@@ -14,10 +14,10 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="./vista/css/layout-nav.css">
   <link rel="stylesheet" href="./vista/css/editor.css">
+  <script src="./vista/js/fileSaver.js"></script>
   <script defer src="./vista/js/puppet-editor.js"></script>
-
   <script defer src="./vista/js/editor_date.js"></script>
-  <script defer src="./vista/js/editor-ExportarExcel.js"></script>
+  <!-- <script defer src="./vista/js/editor-ExportarExcel.js"></script> -->
 </head>
 <body>
   <?php include_once("./vista/layout/nav.php") ?>
--- vista/js/editor_date.js
+++ vista/js/editor_date.js
@@ -1,3 +1,6 @@
+// const { saveAs } = require("./vista/js/fileSaver.js");
+
+var informacion_archivos;
 $(document).ready(function(){
     carga_tablero();
     let activo = true;
@@ -15,14 +18,20 @@ $(document).ready(function(){
             success: function(response){
                 // console.log('Respuesta del servidor:', response);
                 if(response != ""){
-                    const datos = JSON.parse(response);
-                    vista_previa_datos(datos,"tabla_datos_principal"); 
+                    const datos1 = JSON.parse(response);
+                    informacion_archivos = datos1;
+                    vista_previa_datos(datos1,"tabla_datos_principal"); 
                 }
             }
         })
         // console.log(dato.innerHTML);
 
     }
+
+    $(document).on("click","#btnExportar",function(){
+        descargar_archivos();
+        console.log("clikeado");
+    });
 
     $(document).on("click",".boton_redondo_datos",function(){
         // (activo)?false:true;
@@ -74,4 +83,23 @@ function vista_previa_datos(archivos,id_dato){
     tabla.innerHTML = contenido;
     document.getElementById(id_dato).innerHTML = "";
     document.getElementById(id_dato).appendChild(tabla); 
+}
+function descargar_archivos(){
+    const EXCEL_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+    const EXCEL_EXTENCION = ".xlsx";
+    const worshet = XLSX.utils.json_to_sheet(informacion_archivos);
+    const workbook = {Sheets:{
+        'data':worshet
+    },SheetNames:['data']};
+
+    const excelBuffer = XLSX.write(workbook,{bookType:"xlsx",type:"array"});
+    console.log(excelBuffer);
+    guardar_excel(excelBuffer,"myfile_prueba");
+}
+function guardar_excel(buffer,filename){
+    const EXCEL_EXTENCION = ".xlsx";
+    const EXCEL_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+
+    const data = new Blob([buffer],{type:EXCEL_TYPE});
+    saveAs(data,filename + new Date().getTime()+EXCEL_EXTENCION);
 }
