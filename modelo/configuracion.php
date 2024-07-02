e48392ada19525bc6b4b44cae90e9fa48d35e3dc
luan-nee <luandelsol54@gmail.com>
2024-07-01 21:06:55 -0500

    Ingresando credenciales para la conexión a la base de datos

--- modelo/configuracion.php
+++ modelo/configuracion.php
@@ -1,5 +1,10 @@
 <?php
-        define("password","");
-        define("user","root");
-        define("bd_date","ficheros");
+// SERVIDOR LOCAL
+define("password","");
+define("user","root");
+define("bd_date","ficheros");
+// SERVIDOR 000webhost
+define("password","LaContraseniaQueQuieras123$");
+define("user","id22387592_nombrecualquiera123");
+define("bd_date","ficheros");
 ?>
