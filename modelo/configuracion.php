3ac56759beea4e7a8827ddc782ae037cdcbb244a
luan-nee <luandelsol54@gmail.com>
2024-07-01 21:19:06 -0500

    comentando credenciales

--- modelo/configuracion.php
+++ modelo/configuracion.php
@@ -4,7 +4,7 @@ define("password","");
 define("user","root");
 define("bd_date","ficheros");
 // SERVIDOR 000webhost
-define("password","LaContraseniaQueQuieras123$");
-define("user","id22387592_nombrecualquiera123");
-define("bd_date","ficheros");
+// define("password","LaContraseniaQueQuieras123$");
+// define("user","id22387592_nombrecualquiera123");
+// define("bd_date","ficheros");
 ?>
