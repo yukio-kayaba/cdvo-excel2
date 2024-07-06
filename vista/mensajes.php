1601032883bdcbaebff83e7e8e12d440db8b109f
luan del sol <luandelsol54@gmail.com>
2024-07-06 09:55:34 -0500

    subiendo cambios a la web

--- vista/mensajes.php
+++ vista/mensajes.php
@@ -2,8 +2,8 @@
   <title>CDVO || Mensajes</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="./vista/css/layout-nav.css">
-  <link rel="stylesheet" href="<?php echo url;?>/vista/css/mensajes_e.css">
-  <link rel="stylesheet" href="<?php echo url;?>/vista/css/components/mensajes_estylos.css">
+  <link rel="stylesheet" href="./vista/css/mensajes_e.css">
+  <link rel="stylesheet" href="./vista/css/components/mensajes_estylos.css">
   <script defer src="./vista/js/notificaciones.js"></script>
   <script defer src="./vista/js/mensajes_vista.js"></script>
 </head>
